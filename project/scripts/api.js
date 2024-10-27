82d5d363afee493d1f785dc617eddcadd123e532
Kaan Özkan <rahipdotaci@gmail.com>
2024-10-27 21:22:52 +0300

    creation of the initial structure

---
+++ project/scripts/api.js
@@ -0,0 +1,23 @@
+// api.js
+class API {
+    constructor() {
+        this.ngrokUrl = 'YOUR_NGROK_URL';
+    }
+
+    async sendMessage(text) {
+        const response = await fetch(`${this.ngrokUrl}/chat`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: text })
+        });
+        
+        return await response.json();
+    }
+
+    async getTradeData() {
+        const response = await fetch(`${this.ngrokUrl}/trade-data`);
+        return await response.json();
+    }
+}
